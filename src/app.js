a0d9b7f26fce574a1305c8af6d94902d8521078e
mehdigoom <medhi.gay@sfr.fr>
2019-07-31 15:24:02 +0200

    Correction message edge

--- src/app.js
+++ src/app.js
@@ -1134,7 +1134,7 @@ if(this._isInCombineMode){
             var version = parseFloat(testVersion[1]);
             var testEdge = window.navigator.userAgent.match(/Edge\/\d+/); // on teste si le useragent contient Edge
             if(version >= 10.0 && testEdge){
-               alert("Oups ! Ton navigateur n'est pas totalment compatible avec notre platforme. Merci d'utilisé Google Chrome ou Firefox. Vous pouvez toute fois vous servire de la platforme. Cela rique de causé des bugs. Promis c'est pas de notre faute ! ;)")
+               alert("Oups ! Ton navigateur n'est pas totalment compatible avec notre platforme. Merci d'utilisé Google Chrome, Firefox, Safarie.  Promis c'est pas de notre faute ! ;)")
                
             }
         }
