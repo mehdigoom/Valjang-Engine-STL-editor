8495838c99b2a7d1548ec9e81d1c2f6c2f317e4b
mehdigoom <medhi.gay@sfr.fr>
2019-08-20 16:19:55 +0200

    page de base

--- admin/admin-panel/src/App.js
+++ admin/admin-panel/src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
   return (
+
     <div className="App">
+    <header>
+    <p>panel admin</p>
+    </header>
      
+ 
      
-     
-     <p>panel admin</p>
+     <form>
+  <label>
+    Login:
+    <input type="text" name="name" />
+    Password :
+    <input type="password" name="Password" />
+  </label>
+  <input type="submit" value="Envoyer" />
+</form>
+
 
 
 
