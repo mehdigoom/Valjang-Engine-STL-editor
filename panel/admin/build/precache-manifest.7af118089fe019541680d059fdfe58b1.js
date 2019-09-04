7d1d92d3b0c83eecfd8e18f4ab42872a67352ea9
mehdigoom <medhi.gay@sfr.fr>
2019-09-04 12:06:22 +0200

    add build

---
+++ panel/admin/build/precache-manifest.7af118089fe019541680d059fdfe58b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de35b97c2c3041a34e928cdf521db0fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f1624555e7294efebe",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fbf43fb1a3a8cdd97832",
+    "url": "/static/js/2.4077daa8.chunk.js"
+  },
+  {
+    "revision": "d0f1624555e7294efebe",
+    "url": "/static/js/main.9d80840e.chunk.js"
+  },
+  {
+    "revision": "d4f610c653867fedd5b2",
+    "url": "/static/js/runtime~main.5cc32c95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
