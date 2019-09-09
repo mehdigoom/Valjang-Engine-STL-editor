7c3c44addd6c957fc522094c5ae86aaa8176d22d
mehdigoom <medhi.gay@sfr.fr>
2019-09-09 10:08:42 +0200

    correct ip

--- com.js
+++ com.js
@@ -21,10 +21,10 @@ IDclient ="0";
 //Use Backend ? True = Yes, False = no. //If you use the valjang API this option will replace
  Backend = true;
 // Set IP Backend server //If you use the valjang API this option will replace
- ServerBackend = "http://15.188.48.149:5000";
+ ServerBackend = "http://35.180.41.65:5000";
 
  //set uplaud server //If you use the valjang API this option will replace
- Serveruplaud = "http://15.188.48.149";
+ Serveruplaud = "http://35.180.41.65";
 
 //preload model ? true = yes, false = no.
 preload = false;
