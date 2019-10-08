162a8a5c8e6149430c284eeec350c4428d6934b2
mehdigoom <medhi.gay@sfr.fr>
2019-10-08 17:11:34 +0200

    affichage mode dans panel

--- panel/admin/src/panel.js
+++ panel/admin/src/panel.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react'
 import  './App.css'
-
-
-
-
+import Async from 'react-async';
+const model = () =>
+  fetch("http://public.valjang.fr:5000/model")
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
 
 class pan extends Component {
   constructor(props) {
     super(props);
-    
     this.state = {
       IfErr:false,
       mmessage:""
     }
     this.errdetector = this.errdetector.bind(this);
   } 
-
-
-
 }
 
+ 
 
 
 // You can Use this Variable For set Name client. You can Set "BDD" for use Sql DataBase Client
@@ -34,19 +32,58 @@ var ServerBackend = "http://public.valjang.fr:5000";
 var Serveruplaud = "http://public.valjang.fr";
 
 
+
+
+
+
+
+
+
+
+
+
 function panel(clientID,client,login) {
 
   return (
     <div className="App">
-      <header className="App-header">      
- <p>Panel de {login}.</p>
+      
+      <Async promiseFn={model}>
+        {({ data, err, isLoading }) => {
+          if (isLoading) return "Loading..."
+          if (err) return `Something went wrong: ${err.message}`
+
+          if (data)
+            return (
+              <div>
+                <div>
+                <input class="favorite styled" type="button" name="Loginbtn" value="Ajouter un model" />
+               
+                </div>
+                {data.map(user=> (
+                  <div key={user.username} className="row">
+                    <div className="col-md-12">
+                    <h1>{user.name}</h1>
+                    <img class="fit-picture"src={user.image}/>
+                    <br></br>
+                    <input class="favorite styled" type="button" name="Loginbtn" value="Modifier" />
+                    <input class="favorite styled" type="button" name="Loginbtn" value="Supprimer" />
+                    <input class="favorite styled" type="button" name="Loginbtn" value="Voir dans le viwver" />
+                      
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )
+        }}
+      </Async>
+    </div>
  
 
 
 
 
-      </header>
-    </div>
+      
+  
   );
 }
 
