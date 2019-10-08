57e8f1fe6a5d4c84320c1f7d82e05de9d4ea0dc1
mehdigoom <medhi.gay@sfr.fr>
2019-10-08 17:29:06 +0200

    panel

--- panel/admin/src/panel.js
+++ panel/admin/src/panel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import  './App.css'
 import Async from 'react-async';
 const model = () =>
   fetch("http://public.valjang.fr:5000/model")
@@ -17,22 +16,38 @@ class pan extends Component {
   } 
 }
 
- 
 
+function Getliste() {
+  return(      <Async promiseFn={model}>
+    {({ data, err, isLoading }) => {
+      if (isLoading) return "Loading..."
+      if (err) return `Something went wrong: ${err.message}`
 
-// You can Use this Variable For set Name client. You can Set "BDD" for use Sql DataBase Client
-var NameOfClient = "BDD";
-// If you use the Database for your client name, please set Your ID client
-var IDclient = "0";
-
-// Set IP Backend server //If you use the valjang API this option will replace
-var ServerBackend = "http://public.valjang.fr:5000";
-
-//set uplaud server //If you use the valjang API this option will replace
-var Serveruplaud = "http://public.valjang.fr";
-
-
-
+      if (data)
+        return (
+          <div>
+            <div>
+            <input class="favorite styled" type="button" name="Loginbtn" value="Ajouter un model" />
+           
+            </div>
+            {data.map(user=> (
+              <div key={user.username} className="row">
+                <div className="col-md-12">
+                <h1>{user.name}</h1>
+                <img class="fit-picture"src={user.image} />
+                <br></br>
+                <input class="favorite styled" type="button" name="Loginbtn" value="Modifier" />
+                <input class="favorite styled" type="button" name="Loginbtn" value="Supprimer" />
+                <input class="favorite styled" type="button" name="Loginbtn" value="Voir dans le viwver" />
+                  
+                </div>
+              </div>
+            ))}
+          </div>
+        )
+    }}
+  </Async>)
+}
 
 
 
@@ -46,44 +61,8 @@ function panel(clientID,client,login) {
 
   return (
     <div className="App">
-      
-      <Async promiseFn={model}>
-        {({ data, err, isLoading }) => {
-          if (isLoading) return "Loading..."
-          if (err) return `Something went wrong: ${err.message}`
-
-          if (data)
-            return (
-              <div>
-                <div>
-                <input class="favorite styled" type="button" name="Loginbtn" value="Ajouter un model" />
-               
-                </div>
-                {data.map(user=> (
-                  <div key={user.username} className="row">
-                    <div className="col-md-12">
-                    <h1>{user.name}</h1>
-                    <img class="fit-picture"src={user.image}/>
-                    <br></br>
-                    <input class="favorite styled" type="button" name="Loginbtn" value="Modifier" />
-                    <input class="favorite styled" type="button" name="Loginbtn" value="Supprimer" />
-                    <input class="favorite styled" type="button" name="Loginbtn" value="Voir dans le viwver" />
-                      
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )
-        }}
-      </Async>
-    </div>
- 
-
-
-
-
-      
-  
+      {Getliste()}
+    </div> 
   );
 }
 
