95c828742b0e1cfb0302847e4c035e73b7473042
mehdigoom <medhi.gay@sfr.fr>
2020-02-20 13:28:11 +0100

    fix sql

--- backend/database.js
+++ backend/database.js
@@ -158,7 +158,7 @@ const addBasket = function addBasket(clbk, id,quantity,products_id,users_id) {
 }
 
 const addUser = function addUser(clbk, user) {
-  let sql = "INSERT INTO table users (?,?,?,?)"
+  let sql = "INSERT INTO users VALUES (?,?,?,?)"
   const payload = [user.firstname, user.lastname, user.mail,user.password]
   client.query(sql, payload, function (err, res) {
     if (err) return clbk(err, null);
