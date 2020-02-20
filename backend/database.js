34bea76c6487b408d8a1be49d38cdeab348956a0
mehdigoom <medhi.gay@sfr.fr>
2020-02-20 13:22:44 +0100

    test sql

--- backend/database.js
+++ backend/database.js
@@ -159,7 +159,7 @@ const addBasket = function addBasket(clbk, id,quantity,products_id,users_id) {
 
 const addUser = function addUser(clbk, user) {
   let sql = "INSERT INTO table VALUES (?,?,?,?,?)"
-  const payload = [user.firstname, user.lastname, user.email, user.id]
+  const payload = [user.firstname, user.lastname, user.mail,user.password]
   client.query(sql, payload, function (err, res) {
     if (err) return clbk(err, null);
     return clbk(null, res)
