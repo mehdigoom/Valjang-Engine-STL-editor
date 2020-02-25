7def99a126b9a74ca10c89bc3cfff3bfe8f2eda4
mehdigoom <medhi.gay@sfr.fr>
2020-02-25 11:35:16 +0100

    Test react to apache

--- panel/panel/src/App.js
+++ panel/panel/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+function init(){
+return("Loading...")
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="wrapper fadeInDown">
+      <header className="formContent">
+        
+        <h1 id="Load">
+          STL editor
+        </h1>
+        <a>
+        {init()}
         </a>
       </header>
     </div>
